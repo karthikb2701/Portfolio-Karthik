4e7c33ba94260a3d0ca34d7d19245abe405d24ec
Karthikb2701@ <karthikb2701@gmail.com>
2025-07-12 21:56:13 +0530

    third commit

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 
 export default function useDarkMode() {
-  const [theme, setTheme] = useState('light');
+  const [theme, setTheme] = useState('dark');
 
   useEffect(() => {
     const root = document.documentElement;
